06ca3dbfeba9eed2aceaf3468a153c69a9c503f0
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-11-26 16:43:54 -0800

    Serve static files at root

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
